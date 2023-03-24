ff0772b28850436a5cc5a0a8505310c0d37cba85
Neikan <sladkov.e.m@outlook.com>
2023-03-24 18:37:14 +0300

    feat(project): add mock-data to hotels

--- lib/data/repositories/repository_hotels/repository_hotels_imp.dart
+++ lib/data/repositories/repository_hotels/repository_hotels_imp.dart
@@ -1,24 +1,30 @@
 // Project imports:
-import 'package:app_perfomance/data/consts/urls.dart';
+import 'package:app_perfomance/data/mock/data.dart';
 import 'package:app_perfomance/data/models/api_hotel.dart';
 import 'package:app_perfomance/data/repositories/repository_hotels/repository_hotels.dart';
-import 'package:app_perfomance/data/services/service_http.dart';
 
 class RepositoryHotelsImp extends RepositoryHotels {
   const RepositoryHotelsImp();
 
+  // @override
+  // Future<List<ApiHotel>> getData() async {
+  //   List<ApiHotel> hotels = [];
+
+  //   final response = await ServiceHttp.instance.get(urlHotels);
+
+  //   if (response.statusCode == 200) {
+  //     hotels = List.from(response.data)
+  //         .map((hotel) => ApiHotel.fromJson(hotel))
+  //         .toList();
+  //   }
+
+  //   return hotels;
+  // }
+
   @override
   Future<List<ApiHotel>> getData() async {
-    List<ApiHotel> hotels = [];
-
-    final response = await ServiceHttp.instance.get(urlHotels);
-
-    if (response.statusCode == 200) {
-      hotels = List.from(response.data)
-          .map((hotel) => ApiHotel.fromJson(hotel))
-          .toList();
-    }
-
-    return hotels;
+    return List.from(rawHotels)
+        .map((hotel) => ApiHotel.fromJson(hotel))
+        .toList();
   }
 }
